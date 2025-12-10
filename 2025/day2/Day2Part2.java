1fc505a56688eb33c7a146e7e4d7867c6b6f4559
Malte Berg <malte@malte.nu>
2025-12-10 22:52:24 +0100

    Implement Day 2 Part 2 in Java

---
+++ 2025/day2/Day2Part2.java
@@ -0,0 +1,54 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Day2Part2 {
+
+  public static void main(String[] args) {
+    File inputFile = new File("input.txt");
+
+    try {
+      Scanner fileReader = new Scanner(inputFile);
+
+      String input = fileReader.nextLine();
+      String[] ranges = input.split(",");
+
+      long sum = 0;
+
+      for (String range : ranges) {
+
+        long startOfRange = Long.parseLong(range.split("-")[0]);
+        long endOfRange = Long.parseLong(range.split("-")[1]);
+
+        for (long i = startOfRange; i <= endOfRange; i++) {
+
+          String s = Long.toString(i);
+          int length = s.length();
+
+          for (int sub = 1; sub <= length/2; sub++) {
+
+            if (length % sub != 0) continue;
+
+            String substring = s.substring(0, sub);
+            String regex = "(?i)".concat(substring);
+            String replaced = s.replaceAll(regex, "");
+
+            if (replaced.length() == 0) {
+              sum += i;
+              break;
+            }
+          }
+
+        }
+
+      }
+
+      System.out.println("Total sum: " + sum);
+
+    } catch (FileNotFoundException e) {
+      System.out.println("error");
+    }
+
+  }
+
+}
