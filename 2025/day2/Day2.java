4d40dc3a91b9350a89e933c00512f4b330ef4515
Malte Berg <malte@malte.nu>
2025-12-10 22:21:29 +0100

    Implement Day 2 in Java

---
+++ 2025/day2/Day2.java
@@ -0,0 +1,45 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Day2 {
+
+  public static void main(String[] args) {
+    File inputFile = new File("input.txt");
+
+    try {
+      Scanner fileReader = new Scanner(inputFile);
+
+      String input = fileReader.nextLine();
+      String[] ranges = input.split(",");
+
+      long sum = 0;
+
+      for (String range : ranges) {
+
+        long startOfRange = Long.parseLong(range.split("-")[0]);
+        long endOfRange = Long.parseLong(range.split("-")[1]);
+
+        for (long i = startOfRange; i <= endOfRange; i++) {
+
+          String s = Long.toString(i);
+          int length = s.length();
+
+          if (length % 2 != 0) continue;
+
+          if(s.substring(0, length/2).compareTo(s.substring(length/2, length)) == 0)
+            sum += i;
+
+        }
+
+      }
+
+      System.out.println("Total sum: " + sum);
+
+    } catch (FileNotFoundException e) {
+      System.out.println("error");
+    }
+
+  }
+
+}
